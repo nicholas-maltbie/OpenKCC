a655bd2df9438451734442e6bcff3dc95ce9ab70
Nicholas Maltbie <nick.dmalt@gmail.com>
2022-01-11 14:08:01 -0800

    Updated editorconfig for quick outline license header

--- Assets/OpenKCC/Shaders/QuickOutline/Scripts/Outline.cs
+++ Assets/OpenKCC/Shaders/QuickOutline/Scripts/Outline.cs
@@ -1,24 +1,25 @@
-﻿// Copyright (C) 2022 Nicholas Maltbie
+﻿// Copyright (c) 2018 Chris Nolet
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-// associated documentation files (the "Software"), to deal in the Software without restriction,
-// including without limitation the rights to use, copy, modify, merge, publish, distribute,
-// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is furnished to do
+// so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all copies or
-// substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace QuickOutline
@@ -81,160 +82,14 @@ namespace QuickOutline
 
         [SerializeField, Range(0f, 10f)]
         private float outlineWidth = 2f;
-
-        [Header("Optional")]
-
-        [SerializeField, Tooltip("Precompute enabled: Per-vertex calculations are performed in the editor and serialized with the object. "
-        + "Precompute disabled: Per-vertex calculations are performed at runtime in Awake(). This may cause a pause for large meshes.")]
-        private bool precomputeOutline;
-
         [SerializeField, HideInInspector]
         private List<Mesh> bakeKeys = new List<Mesh>();
 
         [SerializeField, HideInInspector]
         private List<ListVector3> bakeValues = new List<ListVector3>();
-
-        private Renderer[] renderers;
-        private Material outlineMaskMaterial;
-        private Material outlineFillMaterial;
-
         private bool needsUpdate;
 
         public const string OUTLINE_MASK_SHADER = "Custom/Outline Mask";
         public const string OUTLINE_FILL_SHADER = "Custom/Outline Fill";
-
-        private void Bake()
-        {
-
-            // Generate smooth normals for each mesh
-            var bakedMeshes = new HashSet<Mesh>();
-
-            foreach (MeshFilter meshFilter in GetComponentsInChildren<MeshFilter>())
-            {
-
-                // Skip duplicates
-                if (!bakedMeshes.Add(meshFilter.sharedMesh))
-                {
-                    continue;
-                }
-
-                // Serialize smooth normals
-                List<Vector3> smoothNormals = SmoothNormals(meshFilter.sharedMesh);
-
-                bakeKeys.Add(meshFilter.sharedMesh);
-                bakeValues.Add(new ListVector3() { data = smoothNormals });
-            }
-        }
-
-        private void LoadSmoothNormals()
-        {
-
-            // Retrieve or generate smooth normals
-            foreach (MeshFilter meshFilter in GetComponentsInChildren<MeshFilter>())
-            {
-
-                // Skip if smooth normals have already been adopted
-                if (!registeredMeshes.Add(meshFilter.sharedMesh))
-                {
-                    continue;
-                }
-
-                // Retrieve or generate smooth normals
-                int index = bakeKeys.IndexOf(meshFilter.sharedMesh);
-                List<Vector3> smoothNormals = (index >= 0) ? bakeValues[index].data : SmoothNormals(meshFilter.sharedMesh);
-
-                // Store smooth normals in UV3
-                meshFilter.sharedMesh.SetUVs(3, smoothNormals);
-            }
-
-            // Clear UV3 on skinned mesh renderers
-            foreach (SkinnedMeshRenderer skinnedMeshRenderer in GetComponentsInChildren<SkinnedMeshRenderer>())
-            {
-                if (registeredMeshes.Add(skinnedMeshRenderer.sharedMesh))
-                {
-                    skinnedMeshRenderer.sharedMesh.uv4 = new Vector2[skinnedMeshRenderer.sharedMesh.vertexCount];
-                }
-            }
-        }
-
-        private List<Vector3> SmoothNormals(Mesh mesh)
-        {
-
-            // Group vertices by location
-            IEnumerable<IGrouping<Vector3, KeyValuePair<Vector3, int>>> groups = mesh.vertices.Select((vertex, index) => new KeyValuePair<Vector3, int>(vertex, index)).GroupBy(pair => pair.Key);
-
-            // Copy normals to a new list
-            var smoothNormals = new List<Vector3>(mesh.normals);
-
-            // Average normals for grouped vertices
-            foreach (IGrouping<Vector3, KeyValuePair<Vector3, int>> group in groups)
-            {
-
-                // Skip single vertices
-                if (group.Count() == 1)
-                {
-                    continue;
-                }
-
-                // Calculate the average normal
-                Vector3 smoothNormal = Vector3.zero;
-
-                foreach (KeyValuePair<Vector3, int> pair in group)
-                {
-                    smoothNormal += mesh.normals[pair.Value];
-                }
-
-                smoothNormal.Normalize();
-
-                // Assign smooth normal to each vertex
-                foreach (KeyValuePair<Vector3, int> pair in group)
-                {
-                    smoothNormals[pair.Value] = smoothNormal;
-                }
-            }
-
-            return smoothNormals;
-        }
-
-        private void UpdateMaterialProperties()
-        {
-
-            // Apply properties according to mode
-            outlineFillMaterial.SetColor("_OutlineColor", outlineColor);
-            outlineMaskMaterial.SetColor("_OutlineColor", outlineColor);
-
-            switch (outlineMode)
-            {
-                case Mode.OutlineAll:
-                    outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
-                    outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
-                    outlineFillMaterial.SetFloat("_OutlineWidth", outlineWidth);
-                    break;
-
-                case Mode.OutlineVisible:
-                    outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
-                    outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
-                    outlineFillMaterial.SetFloat("_OutlineWidth", outlineWidth);
-                    break;
-
-                case Mode.OutlineHidden:
-                    outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
-                    outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Greater);
-                    outlineFillMaterial.SetFloat("_OutlineWidth", outlineWidth);
-                    break;
-
-                case Mode.OutlineAndSilhouette:
-                    outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
-                    outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Always);
-                    outlineFillMaterial.SetFloat("_OutlineWidth", outlineWidth);
-                    break;
-
-                case Mode.SilhouetteOnly:
-                    outlineMaskMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.LessEqual);
-                    outlineFillMaterial.SetFloat("_ZTest", (float)UnityEngine.Rendering.CompareFunction.Greater);
-                    outlineFillMaterial.SetFloat("_OutlineWidth", 0f);
-                    break;
-            }
-        }
     }
 }
