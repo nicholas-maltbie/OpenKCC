bf5c02d2531a95aba9d88a559d2c54ec416b57de
Nick Maltbie <nick.dmalt@gmail.com>
2022-05-28 08:37:05 -0700

    Fixed small error in build script for website

--- Documentation/build.sh
+++ Documentation/build.sh
@@ -1,7 +1,7 @@
 BASEDIR=$(dirname "$0")
 
 # Setup files for website
-cp $BASEDIR/../README.md index.md
+cp $BASEDIR/../README.md $BASEDIR/index.md
 
 # Generate website with docfx
 docfx metadata $BASEDIR/docfx.json --warningsAsErrors --logLevel verbose
