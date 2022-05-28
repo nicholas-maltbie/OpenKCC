69af96e7bf34a7843aabaf0990b30dd480a861fd
Nick Maltbie <nick.dmalt@gmail.com>
2022-05-28 13:44:19 -0700

    Corrected build to exit properly if documentation has errors

--- Documentation/build.cmd
+++ Documentation/build.cmd
@@ -1,12 +1,22 @@
 @REM Setup files for website
-echo "Setting up website and copying files"
+echo Setting up website and copying files
 copy %~dp0\..\README.md %~dp0\index.md
 copy %~dp0\..\LICENSE.txt %~dp0\LICENSE.txt
 xcopy /E /S /Y %~dp0\..\Demo %~dp0\Demo\
 
 @REM Generate website with docfx
-echo "Building code metadata"
-docfx metadata %~dp0\docfx.json --warningsAsErrors --logLevel verbose --force
+echo Building code metadata
+docfx metadata %~dp0\docfx.json --warningsAsErrors --logLevel verbose --force && (
+    echo Successfuly generated metadata for C# code formatting
+) || (
+    echo Could not properly generate metadata for C# code formatting
+    exit /b 1
+)
 
-echo "Generating website"
-docfx build %~dp0\docfx.json --warningsAsErrors --logLevel verbose
+echo Generating website
+docfx build %~dp0\docfx.json --warningsAsErrors --logLevel verbose && (
+    echo Successfuly generated website for documentation
+) || (
+    echo Could not properly website for documentation
+    exit /b 1
+)
