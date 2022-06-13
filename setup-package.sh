b9f36d788e9d88d8929e91c696821b4cd6c71749
Nick Maltbie <nick.dmalt@gmail.com>
2022-06-13 03:52:08 -0700

    Add Version Control to Release Branches (#63)
    * Attempting to use git lfs on push
    
    * Corrected package definition
    
    * Updated deploy workflow to not squash history
    
    * Updated setup package workflow
    
    * Adjsuted setup package workflow
    
    * Adjsuted setup package workflow
    
    * Adjsuted setup package workflow
    
    * Adjusted setup script to move files around
    
    * Updated deploy workflow to use lfs files
    
    * Updated setup package script
    
    * Updated setup-package workflow
    
    * Attempted to update commit workflow again
    
    * Attempted to fix package push errors
    
    * updated setup package script to use splittree
    
    * Updated setup package script to use a keep temporary directory
    
    * Attempted to avoid errors in git setup package
    
    * restored deploy workflow original behavior
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- setup-package.sh
+++ setup-package.sh
@@ -1,2 +1,15 @@
 # Sets up unity package samples
-cp -r ./Assets/Samples ./Packages/com.nickmaltbie.openkcc/Samples~
+git mv ./Assets/Samples ./Packages/com.nickmaltbie.openkcc/Samples~
+git mv ./Packages/com.nickmaltbie.openkcc _keep
+
+git lfs install
+
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
+
+git commit -m "Moved ./Assets/Samples to ./Packages/com.nickmaltbie.openkcc/Samples~"
+
+# Cleanup any files not part of the package
+git subtree split --prefix _keep --branch cleaned-branch
+
+git checkout cleaned-branch
