9cca435af61105e3c2de25e8eb7323c6c34d8647
Nick Maltbie <nick.dmalt@gmail.com>
2022-06-13 02:58:07 -0700

    Adjsuted setup package workflow

--- setup-package.sh
+++ setup-package.sh
@@ -5,4 +5,7 @@ git add ./Packages/com.nickmaltbie.openkcc/Samples~
 
 git lfs install
 
-git commit -m "Moved ./Assets/Samples to ./Packages/com.nickmaltbie.openkcc/Samples~" --author="github-actions[bot] <github-actions[bot]@users.noreply.github.com>"
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
+
+git commit -m "Moved ./Assets/Samples to ./Packages/com.nickmaltbie.openkcc/Samples~"
