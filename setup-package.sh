608d8267acffdab95519c753e6a81ab45da0e292
Nick Maltbie <nick.dmalt@gmail.com>
2022-06-13 02:52:26 -0700

    Updated deploy workflow to not squash history

--- setup-package.sh
+++ setup-package.sh
@@ -1,2 +1,6 @@
 # Sets up unity package samples
 mv ./Assets/Samples ./Packages/com.nickmaltbie.openkcc/Samples~
+
+git add ./Packages/com.nickmaltbie.openkcc/Samples~
+
+git commit -m "Moved ./Assets/Samples to ./Packages/com.nickmaltbie.openkcc/Samples~"
