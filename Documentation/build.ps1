264297b6100f0d5ec41a60800a887fe277687cfe
Nick Maltbie <nick.dmalt@gmail.com>
2023-01-29 12:59:18 -0800

    small update to how parent path is computed for build.ps1 doc script

--- Documentation/build.ps1
+++ Documentation/build.ps1
@@ -44,7 +44,7 @@ foreach ($tag in ('v0.0.61', 'v0.1.0', 'v0.1.2', 'v1.0.0', 'v1.1.0', 'v1.2.0'))
 
         if (Test-Path "$project_dir\$path")
         {
-            $parent = $(Get-Item "$dir\versions\$tag\$path").Parent
+            $parent = Split-Path -parent "$dir\versions\$tag\$path"
             if (!Test-Path "$project_dir\$path")
             {
                 New-Item -Path "$parent" -ItemType Directory
