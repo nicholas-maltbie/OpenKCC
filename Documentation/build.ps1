f5983a55c8edd769c67a006903f4bd90f9d2c8c5
Nick Maltbie <nick.dmalt@gmail.com>
2023-01-29 13:54:45 -0800

    Overhaul to build script fixing many errors

--- Documentation/build.ps1
+++ Documentation/build.ps1
@@ -14,43 +14,45 @@ Copy-Item -Force "$project_dir\README.md" "$dir\index.md"
 Copy-Item -Force "$project_dir\LICENSE.txt" "$dir\LICENSE.txt"
 Copy-Item -Force "$project_dir\Packages\com.nickmaltbie.openkcc\CHANGELOG.md" "$dir\changelog\CHANGELOG.md"
 Copy-Item -Force "$project_dir\Packages\com.nickmaltbie.openkcc.netcode\CHANGELOG.md" "$dir\changelog\CHANGELOG.netcode.md"
-Copy-Item -Recurse -Force "$project_dir\Demo" "$dir\Demo\"
+Copy-Item "$project_dir\Demo" "$dir\Demo\"-Recurse -Force 
 
-$doc_paths = @("Packages", "Assets\Samples", "Documentation\manual", "Documentation\resources")
+$doc_paths = @("Packages", "Assets/Samples", "Documentation/manual", "Documentation/resources")
 
 # Cleanup any previous documentation
-if (Test-Path "$dir\$versions")
+if (Test-Path "$dir\versions")
 {
-    Remove-Item -Force -Recurse "$dir\$versions" | Out-Null
+    Remove-Item "$dir\versions" -Force -Recurse > $null
 }
 
 # Setup documentation for each version of the api
 foreach ($tag in @('v0.0.61', 'v0.1.0', 'v0.1.2', 'v1.0.0', 'v1.1.0', 'v1.2.0'))
 {
     Write-Host "Setting up docs for version '$tag'"
-
-    New-Item -Path "$dir\versions\$tag" -ItemType Directory
+    New-Item -Path "$dir\versions\$tag" -ItemType Directory > $null
 
     foreach ($path in $doc_paths)
     {
         if (Test-Path "$project_dir\$path")
         {
-            Remove-Item -Force -Recurse "$project_dir\$path" | Out-Null
+            Remove-Item "$project_dir\$path" -Force -Recurse > $null
         }
 
-        git reset "$project_dir\$path" | Out-Null
-        Write-Host "Setting up resources for tag '$tag' at path: '$project_dir\$path'"
-        git checkout "$tag" -- "$project_dir\$path" | Out-Null
-
-        if (Test-Path "$project_dir\$path")
+        # Check if resource exists for path
+        if (git cat-file -t "$($tag):$($path)" && $true || $false)
         {
-            $parent = "$(Split-Path -parent "$dir\versions\$tag\$path")"
-            if (!$(Test-Path "$parent")) 
-            {
-                New-Item -Path "$parent" -Type Directory
-            }
+            Write-Host "Setting up resources for tag '$tag' at path: '$project_dir\$path'"
+            git checkout "$tag" -- "$project_dir/$path" > $null
 
-            Move-Item "$project_dir\$path" "$dir\versions\$tag\$path" | Out-Null
+            if (Test-Path "$project_dir\$path")
+            {
+                $parent = "$(Split-Path -parent "$dir\versions\$tag\$path")"
+                if (!$(Test-Path "$parent")) 
+                {
+                    New-Item -Path "$parent" -Type Directory > $null
+                }
+
+                Move-Item "$project_dir\$path" "$dir\versions\$tag\$path" > $null
+            }
         }
     }
 }
@@ -58,13 +60,15 @@ foreach ($tag in @('v0.0.61', 'v0.1.0', 'v0.1.2', 'v1.0.0', 'v1.1.0', 'v1.2.0'))
 # Restore packages and samples file from master branch
 foreach ($path in $doc_paths)
 {
+    git checkout "$project_dir/$path" > $null
+
     if (Test-Path "$project_dir\$path")
     {
-        Remove-Item -Force -Recurse "$project_dir\$path" | Out-Null
+        git reset "$project_dir/$path" > $null
+        Remove-Item -LiteralPath "$project_dir\$path" -Force -Recurse > $null
     }
 
-    git reset "$project_dir\$path" | Out-Null
-    git checkout "$project_dir\$path" | Out-Null
+    git checkout "$project_dir/$path" > $null
 }
 
 # Generate website with docfx
