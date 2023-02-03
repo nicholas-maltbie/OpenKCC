633a60ac749a7bb35fcd916f0a16520883bbff7c
Nick Maltbie <nick.dmalt@gmail.com>
2023-02-03 00:41:16 -0800

    Version Dropdown Selector for Documentation Website (#160)

---
+++ Documentation/templates/custom/styles/main.js
@@ -0,0 +1,22 @@
+function currentVersion()
+{
+    return document.getElementById("version").getAttribute("version") || "latest"
+}
+
+function versionList()
+{
+    versions = document.getElementById("version").getAttribute("versionlist") || ""
+    return (versions).split(",")
+}
+
+function selectVersion() {
+    selectedVersion = document.getElementById("version-selector").value
+    prefix = currentVersion() == "latest" ? "/../" : "/../../"
+    root = new URL(document.location + prefix + document.querySelector('meta[property="docfx:rel"]').content).href
+
+    rootWithVersion = new URL(document.location.href + "/../" + document.querySelector('meta[property="docfx:rel"]').content).href
+    leaf = window.location.href.substring(rootWithVersion.length)
+
+    versionPath = selectedVersion == "latest" ? "" : selectedVersion + "/"
+    window.location.href = root + versionPath + leaf
+}
