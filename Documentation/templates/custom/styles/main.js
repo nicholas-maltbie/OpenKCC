4c41c5dc19fb598b66b7db27301b57442ee56a2d
Nick Maltbie <nick.dmalt@gmail.com>
2023-02-02 21:46:43 -0800

    updates for testing version selector

--- Documentation/templates/custom/styles/main.js
+++ Documentation/templates/custom/styles/main.js
@@ -1,15 +1,19 @@
 function currentVersion()
 {
-    return document.getElementById("version").version || "latest"
+    return document.getElementById("version").getAttribute("version") || "latest"
 }
 
 function versionList()
 {
-    versions = document.getElementById("version").versionList || ""
+    versions = document.getElementById("version").getAttribute("versionlist") || ""
     return (versions).split(",")
 }
 
 function selectVersion() {
-    var version = document.getElementById("version-selector").value;
-    console.log("Selected version: " + version);
+    selectedVersion = document.getElementById("version-selector").value
+    prefix = currentVersion() == "latest" ? "/../" : "/../../"
+    root = new URL(document.location + prefix + document.querySelector('meta[property="docfx:rel"]').content).href
+
+    versionPath = selectVersion == "latest" ? "" : selectVersion
+    window.location.href = root + "/" + versionPath
 }
