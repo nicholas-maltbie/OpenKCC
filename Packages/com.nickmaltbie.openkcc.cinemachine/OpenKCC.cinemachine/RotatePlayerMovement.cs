9b4aaa7e577bd250a4546c5858b0cbfd6f2c62fb
Nick Maltbie <nick.dmalt@gmail.com>
2023-02-11 03:45:10 -0800

    fixed small bug causing cinemachine to jitter

--- Packages/com.nickmaltbie.openkcc.cinemachine/OpenKCC.cinemachine/RotatePlayerMovement.cs
+++ Packages/com.nickmaltbie.openkcc.cinemachine/OpenKCC.cinemachine/RotatePlayerMovement.cs
@@ -15,33 +15,28 @@ namespace nickmaltbie.OpenKCC.cinemachine
     public class RotatePlayerMovement : MonoBehaviour
     {
         /// <summary>
-        /// Avatar base to rotate.
-        /// </summary>
-        public GameObject avatarBase;
-
-        /// <summary>
         /// Rotation speed in degrees per second.
         /// </summary>
         public float rotationSpeed = 15.0f;
 
-        public void LateUpdate()
+        public void Update()
         {
             KCCStateMachine sm = GetComponent<KCCStateMachine>();
             ICameraControls cameraControls = GetComponent<ICameraControls>();
 
-            // Rotate the avatar to follow the direction of the
-            // player's movement if it's grater than some dead zone
-            // with a given turning rate.
-            Quaternion currentHeading = avatarBase.transform.rotation;
-            Quaternion desiredHeading = Quaternion.LookRotation(cameraControls.PlayerHeading * sm.InputMovement);
-
             if (sm.InputMovement.magnitude > KCCUtils.Epsilon)
             {
+                // Rotate the avatar to follow the direction of the
+                // player's movement if it's grater than some dead zone
+                // with a given turning rate.
+                Quaternion currentHeading = transform.rotation;
+                Quaternion desiredHeading = Quaternion.LookRotation(cameraControls.PlayerHeading * sm.InputMovement);
+
                 // Rotate the current heading towards the desired heading.
                 Quaternion rotatedHeading = Quaternion.RotateTowards(currentHeading, desiredHeading, rotationSpeed * Time.deltaTime);
 
                 // set the avatar rotation to be the new rotated heading.
-                avatarBase.transform.rotation = rotatedHeading;
+                transform.rotation = rotatedHeading;
             }
         }
     }
