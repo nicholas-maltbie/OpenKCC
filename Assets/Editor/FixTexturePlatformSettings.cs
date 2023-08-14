b13f071af7bd3db7948a4ecf26fc236e4383d01d
Nick Maltbie <nick.dmalt@gmail.com>
2023-08-14 11:51:44 -0700

    Fixed asset import errors and sped up reimport process

--- Assets/Editor/FixTexturePlatformSettings.cs
+++ Assets/Editor/FixTexturePlatformSettings.cs
@@ -1,7 +1,25 @@
-﻿using System;
+﻿// Copyright (C) 2023 Nicholas Maltbie
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using System.Collections;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using Unity.EditorCoroutines.Editor;
 using UnityEditor;
 using UnityEngine;
@@ -34,13 +52,30 @@ public class FixTexturePlatformSettings : AssetPostprocessor
 
     public void OnPostprocessTexture(Texture2D texture)
     {
+        // Ignore library assets
+        if (assetPath.StartsWith("Packages") && !assetPath.StartsWith("Packages\com.nickmaltbie.openkcc"))
+        {
+            return;
+        }
+
         Fix($"{assetPath}.meta");
+    }
+
+    private IEnumerator FixRoutine(string metafile)
+    {
+        if (!File.ReadAllText(metafile).Contains("platformSettings:"))
+        {
+            yield return null;
+        }
+
+        Fix(metafile);
     }
 
     private void Fix(string metafile)
     {
         if (!File.ReadAllText(metafile).Contains("platformSettings:"))
         {
+            EditorCoroutineUtility.StartCoroutine(FixRoutine(metafile), this);
             return;
         }
 
@@ -48,7 +83,7 @@ public class FixTexturePlatformSettings : AssetPostprocessor
         string original = File.ReadAllText(metafile);
         StringBuilder meta = new(original);
 
-        Debug.Log("[FixTPS] Checking platform settings...");
+        Debug.Log($"[FixTPS] Checking platform settings for metafile:{metafile}...");
         foreach (string platform in Platforms)
         {
             if (meta.ToString().Contains(platform))
@@ -59,7 +94,7 @@ public class FixTexturePlatformSettings : AssetPostprocessor
                     int insertAt = meta.ToString().LastIndexOf(Settings[^2]) + Settings[^2].Length + 1;
                     meta.Insert(insertAt, Settings[^1] + '\n');
 
-                    Debug.Log($"[FixTPS] Added missing setting to platform '{platform}'");
+                    Debug.Log($"[FixTPS] Added missing setting to platform '{platform}' for metafile:{metafile}");
                 }
             }
             else
@@ -78,15 +113,15 @@ public class FixTexturePlatformSettings : AssetPostprocessor
                 int insertAt = meta.ToString().LastIndexOf(insertAfter) + insertAfter.Length + 1;
                 meta.Insert(insertAt, string.Join('\n', Settings).Replace("{platform}", platform) + '\n');
 
-                Debug.Log($"[FixTPS] Added missing platform '{platform}'");
+                Debug.Log($"[FixTPS] Added missing platform '{platform}' for metafile:{metafile}");
             }
         }
 
         // Save .meta file
-        if (meta.ToString() != original)
+        if (!Regex.Replace(meta.ToString(), @"\s+", "").Equals(
+            Regex.Replace(original, @"\s+", "")))
         {
             File.WriteAllText(metafile, meta.ToString());
-            AssetDatabase.ImportAsset(assetPath, ImportAssetOptions.ForceUpdate);
         }
     }
 }
